cf2d1f61d9b8163d9610c4e01340d8e3993e56d2
Quỳnh Phan <phanquynh437@gmail.com>
2023-12-04 10:12:01 +0700

    Add account to user_account table

--- src/com/raven/form/Login.java
+++ src/com/raven/form/Login.java
@@ -6,6 +6,7 @@ import com.raven.event.EventMessage;
 import com.raven.event.PublicEvent;
 import com.raven.model.Model_Message;
 import com.raven.model.Model_Register;
+import com.raven.model.Model_User_Account;
 import com.raven.service.Service;
 import io.socket.client.Ack;
 
@@ -51,6 +52,10 @@ public class Login extends javax.swing.JPanel {
                         if (os.length > 0) {
                             Model_Message ms = new Model_Message((boolean)os[0], os[1].toString());
                             message.callMessage(ms);
+                            if (ms.isAction()) {
+                                Model_User_Account user = new Model_User_Account(os[2]);
+                                System.out.println(user.getUserID() + " is User ID");
+                            }
                             // call message back when done register
                         }
                     }
---
+++ src/com/raven/model/Model_User_Account.java
@@ -0,0 +1,102 @@
+
+package com.raven.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author quynhphan
+ */
+public class Model_User_Account {
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Model_User_Account(int userID, String userName, String fullName, String address, String dob, String gender, String email) {
+        this.userID = userID;
+        this.userName = userName;
+        this.fullName = fullName;
+        this.address = address;
+        this.dob = dob;
+        this.gender = gender;
+        this.email = email;
+    }
+    
+    public Model_User_Account(Object json) {
+        JSONObject obj = (JSONObject) json;
+        try {
+            userID = obj.getInt("userID");
+            userName = obj.getString("userName");
+            fullName = obj.getString("fullName");
+            address = obj.getString("address");
+            dob = obj.getString("dob");
+            gender = obj.getString("gender");
+            email = obj.getString("email");
+        } catch (JSONException e) {
+            System.err.println(e);
+        }
+    }
+    
+    private int userID;
+    private String userName;
+    private String fullName;
+    private String address;
+    private String dob;
+    private String gender;
+    private String email;
+    
+}
