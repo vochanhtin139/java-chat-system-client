312498a5791a6cad34153c49ae8d2dd2c1c44c12
Vo Chanh Tin <tin.vo.4767@outlook.com>
2024-01-06 12:21:13 +0700

    fixing UI

--- src/com/raven/form/Menu_Left.java
+++ src/com/raven/form/Menu_Left.java
@@ -47,15 +47,15 @@ public class Menu_Left extends javax.swing.JPanel {
                         break;
                     }
                 }
-                if (menuMessage.isSelected()) {
-                    for (Component com : menuList.getComponents()) {
-                        item_People item = (item_People) com;
-                        if (item.getUser().getUserID() == userID) {
-                            item.updateStatus();
-                            break;
-                        }
-                    }
-                }
+//                if (menuMessage.isSelected()) {
+//                    for (Component com : menuList.getComponents()) {
+//                        item_People item = (item_People) com;
+//                        if (item.getUser().getUserID() == userID) {
+//                            item.updateStatus();
+//                            break;
+//                        }
+//                    }
+//                }
             }
 
             @Override
@@ -67,15 +67,15 @@ public class Menu_Left extends javax.swing.JPanel {
                         break;
                     }
                 }
-                if (menuMessage.isSelected()) {
-                    for (Component com : menuList.getComponents()) {
-                        item_People item = (item_People) com;
-                        if (item.getUser().getUserID() == userID) {
-                            item.updateStatus();
-                            break;
-                        }
-                    }
-                }
+//                if (menuMessage.isSelected()) {
+//                    for (Component com : menuList.getComponents()) {
+//                        item_People item = (item_People) com;
+//                        if (item.getUser().getUserID() == userID) {
+//                            item.updateStatus();
+//                            break;
+//                        }
+//                    }
+//                }
             }
         });
         showMessage();
@@ -125,9 +125,7 @@ public class Menu_Left extends javax.swing.JPanel {
 
         jLabel1 = new javax.swing.JLabel();
         menu = new javax.swing.JLayeredPane();
-        menuMessage = new com.raven.component.MenuButton();
-        menuGroup = new com.raven.component.MenuButton();
-        menuBox = new com.raven.component.MenuButton();
+        jLabel3 = new javax.swing.JLabel();
         sp = new javax.swing.JScrollPane();
         menuList = new javax.swing.JLayeredPane();
 
@@ -136,37 +134,32 @@ public class Menu_Left extends javax.swing.JPanel {
         jLabel1.setText("Menu Left note");
 
         menu.setOpaque(true);
-        menu.setLayout(new java.awt.GridLayout(1, 3));
 
-        menuMessage.setIconSelected(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/message_selected.png"))); // NOI18N
-        menuMessage.setIconSimple(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/message.png"))); // NOI18N
-        menuMessage.setSelected(true);
-        menuMessage.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuMessageActionPerformed(evt);
-            }
-        });
-        menu.add(menuMessage);
+        jLabel3.setFont(new java.awt.Font("Helvetica Neue", 1, 18)); // NOI18N
+        jLabel3.setText("Conversations");
 
-        menuGroup.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/group.png"))); // NOI18N
-        menuGroup.setIconSelected(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/group_selected.png"))); // NOI18N
-        menuGroup.setIconSimple(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/group.png"))); // NOI18N
-        menuGroup.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuGroupActionPerformed(evt);
-            }
-        });
-        menu.add(menuGroup);
+        menu.setLayer(jLabel3, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        menuBox.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/box.png"))); // NOI18N
-        menuBox.setIconSelected(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/box_selected.png"))); // NOI18N
-        menuBox.setIconSimple(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/box.png"))); // NOI18N
-        menuBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuBoxActionPerformed(evt);
-            }
-        });
-        menu.add(menuBox);
+        javax.swing.GroupLayout menuLayout = new javax.swing.GroupLayout(menu);
+        menu.setLayout(menuLayout);
+        menuLayout.setHorizontalGroup(
+            menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 270, Short.MAX_VALUE)
+            .addGroup(menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(menuLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jLabel3)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+        menuLayout.setVerticalGroup(
+            menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+            .addGroup(menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(menuLayout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jLabel3)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
 
         sp.setBorder(null);
         sp.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -191,7 +184,7 @@ public class Menu_Left extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
+            .addComponent(menu)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(sp)
@@ -205,7 +198,7 @@ public class Menu_Left extends javax.swing.JPanel {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(menu, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(menu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(sp)
                 .addContainerGap())
@@ -217,41 +210,12 @@ public class Menu_Left extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void menuMessageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuMessageActionPerformed
-        if (!menuMessage.isSelected()) {
-            menuMessage.setSelected(true);
-            menuGroup.setSelected(false);
-            menuBox.setSelected(false);
-            showMessage();
-        }
-    }//GEN-LAST:event_menuMessageActionPerformed
-
-    private void menuGroupActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuGroupActionPerformed
-        if (!menuGroup.isSelected()) {
-            menuMessage.setSelected(false);
-            menuGroup.setSelected(true);
-            menuBox.setSelected(false);
-            showGroup();
-        }
-    }//GEN-LAST:event_menuGroupActionPerformed
-
-    private void menuBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuBoxActionPerformed
-        if (!menuBox.isSelected()) {
-            menuMessage.setSelected(false);
-            menuGroup.setSelected(false);
-            menuBox.setSelected(true);
-            showBox();
-        }
-    }//GEN-LAST:event_menuBoxActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLayeredPane menu;
-    private com.raven.component.MenuButton menuBox;
-    private com.raven.component.MenuButton menuGroup;
     private javax.swing.JLayeredPane menuList;
-    private com.raven.component.MenuButton menuMessage;
     private javax.swing.JScrollPane sp;
     // End of variables declaration//GEN-END:variables
 }
